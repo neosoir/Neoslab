6fe95ab270214b8831d43b60e2522e10d53c5f40
Neo <neo@neoslab.dev>
2025-03-01 22:18:50 -0600

    [add] old assets

---
+++ site/js/loader.js
@@ -0,0 +1,6 @@
+window.onload = function()
+{
+    var loarder__container = document.getElementById('loader');
+    loarder__container.style.visibility = 'hidden';
+    loarder__container.style.opacity = '0';
+}
