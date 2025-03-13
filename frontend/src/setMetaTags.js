a90ef836c9e83a912386b18f57574f7c74309f15
Neo <neo@neoslab.dev>
2025-03-13 16:30:09 -0600

    [add] open graph meta

--- frontend/src/setMetaTags.js
+++ frontend/src/setMetaTags.js
@@ -1,10 +1,17 @@
 const setMetaTags = () => {
   const baseUrl = import.meta.env.VITE_APP_URL;
+  const imageUrl = `${baseUrl}/open-graph.png`;
 
   const metaUrl = document.createElement('meta');
   metaUrl.setAttribute('property', 'og:url');
   metaUrl.setAttribute('content', baseUrl);
+
+  const metaImage = document.createElement('meta');
+  metaImage.setAttribute('property', 'og:image');
+  metaImage.setAttribute('content', imageUrl);
+
   document.head.appendChild(metaUrl);
+  document.head.appendChild(metaImage);
 };
 
 setMetaTags();
